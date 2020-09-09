8db171bb92d21b3b2b7e9e36ce55f39a3e8e7e13
Vov4ikkk <volodymyr.bots@oa.edu.ua>
2020-09-10 01:34:46 +0300

    git commit

---
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public float speed = 10f;
+
+    private Rigidbody2D rigidbody;
+
+
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        float moveX = Input.GetAxis("Horizontal");
+        rigidbody.MovePosition(rigidbody.position + Vector2.right * moveX * speed * Time.deltaTime);
+        
+    }
+}
